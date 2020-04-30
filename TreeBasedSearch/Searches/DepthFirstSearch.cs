33af3e3cf95abd1802ed2516186112d2c324e036
102113528 <102113528@student.swin.edu.au>
2020-05-01 00:41:26 +1000

    Implement Depth-first search

--- TreeBasedSearch/Searches/DepthFirstSearch.cs
+++ TreeBasedSearch/Searches/DepthFirstSearch.cs
@@ -7,6 +7,9 @@ namespace TreeBasedSearch.Searches
         public string Name { get; } = "Depth-first search";
         public Environment Environment { get; }
 
+        private Stack<Node> _frontier;
+        private List<Cell> _visited;
+
         public DepthFirstSearch(Environment environment)
         {
             Environment = environment;
@@ -14,7 +17,57 @@ namespace TreeBasedSearch.Searches
 
         public List<Cell> Search()
         {
-            throw new System.NotImplementedException();
+            Cell initialCell = Environment.GetCellsByState(CellState.Agent)[0];
+            Node currentNode = new Node(initialCell, null);
+
+            _frontier = new Stack<Node>();
+            _visited = new List<Cell>();
+
+            _frontier.Push(currentNode);
+            _visited.Add(currentNode.Data);
+
+            while (_frontier.Count > 0)
+            {
+                currentNode = _frontier.Pop();
+
+                if (currentNode.Data.State == CellState.Goal)
+                {
+                    List<Cell> result = new List<Cell>();
+
+                    while (currentNode != null)
+                    {
+                        result.Insert(0, currentNode.Data);
+                        currentNode = currentNode.Parent;
+                    }
+
+                    return result;
+                }
+
+                if (currentNode.Data.State == CellState.Wall) continue;
+
+                Cell rightCell = Environment.GetCellAt(currentNode.Data.X + 1, currentNode.Data.Y);
+                if (rightCell != null) AddToFrontier(rightCell, currentNode);
+
+                Cell belowCell = Environment.GetCellAt(currentNode.Data.X, currentNode.Data.Y + 1);
+                if (belowCell != null) AddToFrontier(belowCell, currentNode);
+
+                Cell leftCell = Environment.GetCellAt(currentNode.Data.X - 1, currentNode.Data.Y);
+                if (leftCell != null) AddToFrontier(leftCell, currentNode);
+
+                Cell aboveCell = Environment.GetCellAt(currentNode.Data.X, currentNode.Data.Y - 1);
+                if (aboveCell != null) AddToFrontier(aboveCell, currentNode);
+            }
+
+            return null;
+        }
+
+        private void AddToFrontier(Cell cell, Node parent)
+        {
+            if (_visited.Exists(item => item.X == cell.X && item.Y == cell.Y)) return;
+            _visited.Add(cell);
+
+            Node node = new Node(cell, parent);
+            _frontier.Push(node);
         }
     }
 }
